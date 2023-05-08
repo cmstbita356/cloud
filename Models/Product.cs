adbe0290e1ce36ead42925d9e7e43ec84c02f5b9
cmstbita356 <89367622+cmstbita356@users.noreply.github.com>
2023-05-08 09:44:53 +0700

    .

---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace MyASPnet.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Img { get; set; }
+        public string Desc { get; set; }
+
+        public Product()
+        {
+
+        }
+    }
+}
